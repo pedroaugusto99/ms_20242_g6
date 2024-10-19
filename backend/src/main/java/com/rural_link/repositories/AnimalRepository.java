5b854de01cf0170681402b4819e2b615f9e50cc3
Omar <166178053+OmarJawabri@users.noreply.github.com>
2024-10-19 20:06:33 -0300

    Criação de endpoints de filtragem e listagem de animais

--- backend/src/main/java/com/rural_link/repositories/AnimalRepository.java
+++ backend/src/main/java/com/rural_link/repositories/AnimalRepository.java
@@ -1,10 +1,22 @@
 package com.rural_link.repositories;
 
 import com.rural_link.domain.animal.Animal;
+import com.rural_link.domain.fazenda.Fazenda;
+import com.rural_link.dto.animal.AnimalDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    Optional<Animal> findByCodigo(String codigo);
+public interface AnimalRepository extends JpaRepository<Animal, Long>, JpaSpecificationExecutor<Animal> {
+    Optional<Animal> findByCodigoAndFazenda(String codigo, Fazenda fazenda);
+    List<AnimalDTO> findByNomeLikeAndFazenda(String nome, Fazenda fazenda);
+    Page<AnimalDTO> findByFazenda(Fazenda fazenda, Pageable pageable);
+    Optional<Animal> findByIdAndFazenda(Long id, Fazenda fazenda);
+    List<Animal> findAll(Specification<Animal> specification);
 }
+
--- backend/src/main/java/com/rural_link/service/animal/AnimalService.java
+++ backend/src/main/java/com/rural_link/service/animal/AnimalService.java
@@ -7,19 +7,19 @@ import com.rural_link.domain.usuarios.Proprietario;
 import com.rural_link.domain.usuarios.TrabalhadorRural;
 import com.rural_link.domain.usuarios.UserRole;
 import com.rural_link.dto.animal.AnimalDTO;
+import com.rural_link.dto.animal.AnimalPutDTO;
+import com.rural_link.mapper.AnimalMapper;
 import com.rural_link.repositories.AnimalRepository;
-import com.rural_link.repositories.PessoaRepository;
 import com.rural_link.repositories.ProprietarioRepository;
 import com.rural_link.repositories.TrabalhadorRuralRepository;
-import com.rural_link.service.trabalhador.TrabalhadorRuralService;
-import jakarta.transaction.Transactional;
+import com.rural_link.specifications.AnimalQueryFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -40,21 +40,39 @@ public class AnimalService {
     }
 
     public ResponseEntity<Void> salvar(AnimalDTO animalDTO, Pessoa pessoa){
-        if (animalRepository.findByCodigo(animalDTO.codigo()).isPresent()){
+        Fazenda fazenda = encontrarFazendaDoAnimal(pessoa);
+        if (animalRepository.findByCodigoAndFazenda(animalDTO.codigo(), fazenda).isPresent()){
             return ResponseEntity.badRequest().build();
         }
-        animalRepository.save(Animal.builder()
-                .codigo(animalDTO.codigo())
-                .nome(animalDTO.nome())
-                .dataDeNascimento(animalDTO.dataDeNascimento())
-                .codigoDaMae(animalDTO.codigoDaMae())
-                .codigoDoPai(animalDTO.codigoDoPai())
-                .sexo(animalDTO.sexo())
-                .peso(animalDTO.peso())
-                .origem(animalDTO.origem())
-                .raca(animalDTO.raca())
-                .fazenda(encontrarFazendaDoAnimal(pessoa))
-                .build());
+        Animal animal = AnimalMapper.INSTANCE.toAnimal(animalDTO);
+        animal.setFazenda(fazenda);
+        animalRepository.save(animal);
         return ResponseEntity.ok().build();
+    }
+
+    public Page<AnimalDTO> listarTodosAnimaisPaginados(Pageable pageable, Pessoa pessoa){
+        Fazenda fazenda = encontrarFazendaDoAnimal(pessoa);
+        return animalRepository.findByFazenda(fazenda, pageable);
+    }
+
+    public List<AnimalDTO> buscarAnimalPorNome(String nome, Pessoa pessoa){
+        Fazenda fazenda = encontrarFazendaDoAnimal(pessoa);
+        return animalRepository.findByNomeLikeAndFazenda("%"+nome+"%", fazenda);
+    }
+    
+    public List<AnimalDTO> filtrarPorCampos(AnimalQueryFilter animalQueryFilter, Pessoa pessoa){
+        Fazenda fazenda = encontrarFazendaDoAnimal(pessoa);
+        List<Animal> animais = animalRepository.findAll(animalQueryFilter.toSpecification(fazenda));
+        return AnimalMapper.INSTANCE.toAnimalDTO(animais);
+    }
+
+    public AnimalDTO atualizarDadosDoAnimal(AnimalPutDTO animalPutDTO, Pessoa pessoa){
+        Fazenda fazenda = encontrarFazendaDoAnimal(pessoa);
+        Animal animalSalvo = animalRepository.findByIdAndFazenda(animalPutDTO.id(), fazenda).orElseThrow(() -> new RuntimeException("Animal não está cadastrado"));
+        Animal animal = AnimalMapper.INSTANCE.toAnimal(animalPutDTO);
+        animal.setFazenda(fazenda);
+        animal.setId(animalSalvo.getId());
+        Animal animalAtualizado = animalRepository.save(animal);
+        return AnimalMapper.INSTANCE.toAnimalDTO(animalAtualizado);
     }
 }
