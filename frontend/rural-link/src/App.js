52bab422f61f77a8812550cfb7c3934b38a414f5
pedroaugusto99 <166179411+pedroaugusto99@users.noreply.github.com>
2024-10-20 20:37:52 -0300

    Update App.js

--- frontend/rural-link/src/App.js
+++ frontend/rural-link/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import DashboardComponent from './pages/Dashboard';
-import Registrar from './pages/Registro'
+import RegistrarFazenda from './pages/RegistroFazenda'
+import RegistrarProprietario from './pages/RegistroProprietario'
+import RegistrarTrabalhador from './pages/RegistroTrabalhador'
 import ProtectedRouteProprietario from './routes/ProtectedRouteProprietario';
-import UserSelection from '../src/pages/UserSelection';
-import Perfil from '../src/pages/Perfil'; 
+import UserSelection from '../src/pages/UserSelection'; 
 
 export function App(){
     return (
@@ -12,12 +13,14 @@ export function App(){
             <div className="App">
                 <Routes>
                     <Route path="/login" element={<Login />} />
-                    <Route path="/registrar" element={<Registrar />} />
+                    <Route path="/registrarfazenda" element={<RegistrarFazenda />} />
+                    <Route path="/registrarproprietario" element={<RegistrarProprietario />} />
+                    <Route path="/registrartrabalhador" element={<RegistrarTrabalhador />} />
                     <Route element={<ProtectedRouteProprietario/>}>
-                        <Route path="/dashboard" element={<DashboardComponent />} />
+                    <Route path="/dashboard" element={<DashboardComponent />} />
                     </Route>
                     <Route path="/user-selection" element={<UserSelection />} />
-                    <Route path="/perfil" element={<Perfil />} />
+                    <Route path="/perfil" element={<Perfil />
                 </Routes>
             </div>
         </Router>
