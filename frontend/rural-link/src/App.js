2fb7d5ab7b259cb4f3edb757f37f9870510960dd
V1N1C1U5ESPINDOLA <viniciusespindola@discente.ufg.br>
2024-11-17 15:53:34 -0300

    Corrigindo commits

--- frontend/rural-link/src/App.js
+++ frontend/rural-link/src/App.js
@@ -9,9 +9,11 @@ import Perfil from '../src/pages/Perfil';
 import Dashboard from '../src/pages/Dashboard';
 import Faq from '../src/pages/Faq';
 import Fichamento from '../src/pages/Fichamento';
-import RegistrarAnimal from '../src/pages/RegistrarAnimal'
-import FichaAnimal from '../src/pages/FichaAnimal'
-
+import RegistrarAnimal from '../src/pages/RegistrarAnimal';
+import FichaAnimal from '../src/pages/FichaAnimal';
+import EsqueceuSenha from '../src/pages/EsqueceuSenha';
+import EsqueceuSenhaConfEmail from '../src/pages/EsqueceuSenhaConfEmail';
+import EsqueceuSenhaToken from '../src/pages/EsqueceuSenhaToken';
 export function App(){
     
     return (
@@ -37,8 +39,6 @@ export function App(){
                     <Route path="/fichamento" element={<Fichamento/>} />
                     <Route path="/registraranimal" element={<RegistrarAnimal />} />
                     <Route path="/fichaanimal" element={<FichaAnimal/>} />
-                    <Route path="/ficha_pdf" element={<handleGeneratePDF />} />
-
                 </Routes>
             </div>
         </Router>
